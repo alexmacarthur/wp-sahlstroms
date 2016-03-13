82e1b22e506dc5b4c5674d5c783ae7aecba3e1bf
Alex MacArthur <alex@macarthur.me>
2016-03-12 20:55:38 -0600

    Fix Easter egg van.

--- footer.php
+++ footer.php
@@ -7,12 +7,26 @@
 						<li class="footer-svg-holder"><a href="https://www.facebook.com/Sahlstroms/" target="_blank"><?php get_template_part('img/inline','facebook.svg'); ?></a></li>
 						<li><a href="tel:507-629-3734">507-629-3734</a><br><a href="tel:507-829-6645">507-829-6645</a></li>
 					</ul>
-				<address>Copyright © 2015, Sahlstrom’s Heating, Cooling & Refrigeration, Inc., all rights reserved, Designed &amp; Created by <a class="creator-link" href="http://macarthur.me">Alex MacArthur</a></address>
+				<address>Copyright © <?php echo date("Y"); ?>, Sahlstrom’s Heating, Cooling &amp; Refrigeration, Inc., all rights reserved, Designed &amp; Created by <a class="creator-link" href="http://macarthur.me">Alex MacArthur</a></address>
 			</div>
 		</footer>
 
 		<div id="VanHolder">
-			<?php get_template_part('img/inline','van.svg'); ?>
+			<div class="VanHolder-wrapper">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/sahlstrom-heads.png" alt="Sahlstroms Heating and Refrigeration Van">
+				<div id="smoke">
+				  <span class="s0"></span>
+				  <span class="s1"></span>
+				  <span class="s2"></span>
+				  <span class="s3"></span>
+				  <span class="s4"></span>
+				  <span class="s5"></span>
+				  <span class="s6"></span>
+				  <span class="s7"></span>
+				  <span class="s8"></span>
+				  <span class="s9"></span>
+				</div>
+			</div>
 		</div>
 
 		<?php wp_footer(); ?>
