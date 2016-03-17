0255ee7c9722072017e44a5e9afef7b0ce8a5fc5
Alex MacArthur <alex@macarthur.me>
2016-03-16 22:30:36 -0500

    Style improvements.

--- send.php
+++ send.php
@@ -35,7 +35,7 @@
         }
 
         // Set the recipient email address.
-        $recipient = "bensahlstrom@gmail.com,alex@macarthur.me";
+        $recipient = "alex@macarthur.me";
 
         // Set the email subject.
         $subject = "Sahlstroms HVAC Message Submitted";
