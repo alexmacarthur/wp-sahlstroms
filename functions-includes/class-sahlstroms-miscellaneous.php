cbd7dd1293d1071749fbb75a5a7f03ffe6950f9b
Alex MacArthur <alex@macarthur.me>
2016-07-16 08:18:13 -0500

    Add option for email gateway.

--- functions-includes/class-sahlstroms-miscellaneous.php
+++ functions-includes/class-sahlstroms-miscellaneous.php
@@ -22,89 +22,89 @@ class Sahlstroms_Miscellaneous {
 	}
 	
 	public function email_action() {
-        	$name = strip_tags(trim($_POST["name"]));
+    	$name = strip_tags(trim($_POST["name"]));
 		$name = str_replace(array("\r","\n"),array(" "," "),$name);
-	        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-	        $phonenumber = trim($_POST["phonenumber"]);
-	        $address = trim($_POST["address"]);
-	        $citystate = trim($_POST["citystate"]);
-	        $message = trim($_POST["message"]);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $phonenumber = trim($_POST["phonenumber"]);
+        $address = trim($_POST["address"]);
+        $citystate = trim($_POST["citystate"]);
+        $message = trim($_POST["message"]);
 
-	        if ( empty($name) ) {
-	            echo "Please enter at least your first name.";
-	            http_response_code(400);
-	            die();
-	        }
-	
-	        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-	            echo "Please enter a valid email address.";
-	            http_response_code(400);
-	            die();
-	        }
-	
-	        if ( empty($message) ) {
-	            echo "Please enter a message.";
-	            http_response_code(400);
-	            die();
-	        }
-	
+        if ( empty($name) ) {
+            echo "Please enter at least your first name.";
+            http_response_code(400);
+            die();
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Please enter a valid email address.";
+            http_response_code(400);
+            die();
+        }
+
+        if ( empty($message) ) {
+            echo "Please enter a message.";
+            http_response_code(400);
+            die();
+        }
+
 		$users = get_users();
-	        $emailArray = [];
-	        $phoneArray = [];
-	        foreach($users as $user) {
-	        	if(get_the_author_meta('get_messages', $user->ID) === 'yes') {
-	        		array_push($emailArray, $user->data->user_email); 
-	
-	        		$phoneNumber = get_the_author_meta('phone_number', $user->ID);
-	        		if($phoneNumber) {
-	        			$phoneNumber = preg_replace("/\D/", "", $phoneNumber) . '@vtext.com';
-	        			array_push($phoneArray, $phoneNumber); 
-	        		}
-	        	}
-	        }
-	        $recipients = implode(',', $emailArray);
-	        $textRecipients = implode(',', $phoneArray);
-	
-	        $email_headers = "From: $name <$email>\r\n";
-	        $email_headers .= "Reply-To: $email\r\n";
-	        $email_headers .= "MIME-Version: 1.0\r\n";
-	        $email_headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-	      
-	        $text_headers = "From: $name <$email>\r\n";
-	
-	        $subject = "Sahlstroms HVAC Message Submitted";
-	        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>Name:</strong> $name<br></p>";
-	        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>Phone:</strong> $phonenumber<br></p>";
-	        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>Email:</strong> $email<br></p>";
-	        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>Address:</strong> $address<br></p>";
-	        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>City, State:</strong> $citystate<br></p>";
-	        $email_content .= "<br>";
-	        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>Message:</strong><br>$message</p>\n";
-	        
+        $emailArray = [];
+        $phoneArray = [];
+        foreach($users as $user) {
+        	if(get_the_author_meta('get_messages', $user->ID) === 'yes') {
+        		array_push($emailArray, $user->data->user_email); 
 
-		$text_content = "Sahlstroms HVAC Form Submission:\n";
-	        $text_content .= "$name\n";
-	        $text_content .= "$phonenumber\n";
-	        $text_content .= "$email\n";
-	        $text_content .= "$address\n";
+        		$phoneNumber = get_the_author_meta('phone_number', $user->ID);
+        		$emailGateway = get_the_author_meta('email_gateway', $user->ID);
+        		if($phoneNumber && $emailGateway) {
+        			$phoneNumber = preg_replace("/\D/", "", $phoneNumber) . trim($emailGateway);
+        			array_push($phoneArray, $phoneNumber); 
+        		}
+        	}
+        }
+        $recipients = implode(',', $emailArray);
+        $textRecipients = implode(',', $phoneArray);
+
+        $email_headers = "From: $name <$email>\r\n";
+        $email_headers .= "Reply-To: $email\r\n";
+        $email_headers .= "MIME-Version: 1.0\r\n";
+        $email_headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+      
+        $text_headers = "From: $name <$email>\r\n";
+
+        $subject = "Sahlstroms HVAC Message Submitted";
+        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>Name:</strong> $name<br></p>";
+        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>Phone:</strong> $phonenumber<br></p>";
+        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>Email:</strong> $email<br></p>";
+        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>Address:</strong> $address<br></p>";
+        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>City, State:</strong> $citystate<br></p>";
+        $email_content .= "<br>";
+        $email_content .= "<p style='margin: 0; font-size: 15px;'><strong>Message:</strong><br>$message</p>\n";
+        
+		$text_content = "Online Form Submitted:\n";
+        $text_content .= "$name\n";
+        $text_content .= "$phonenumber\n";
+        $text_content .= "$email\n";
+        $text_content .= "$address\n";
 		$text_content .= "-----\n";
-	        $text_content .= "$message";
-	
-	        mail($textRecipients, '', $text_content, $text_headers);
-	
-	        if (mail($recipients, $subject, $email_content, $email_headers)) {
-	            http_response_code(200);
-	            echo "Thanks! Your message has been sent.";
-	        } else {
-	            http_response_code(500);
-	            echo "Oops! Something went wrong and we couldn't send your message.";
-	        }
-	
-	       	if(empty($_SERVER['HTTP_X_REQUESTED_WITH']) || !strtolower($_SERVER['HTTP_X_REQUESTED_WITH'])){
+        $text_content .= "$message";
+
+        mail($textRecipients, '', $text_content, $text_headers);
+
+        if (mail($recipients, $subject, $email_content, $email_headers)) {
+            http_response_code(200);
+            echo "Thanks! Your message has been sent.";
+        } else {
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+
+       	if(empty($_SERVER['HTTP_X_REQUESTED_WITH']) || !strtolower($_SERVER['HTTP_X_REQUESTED_WITH'])){
 			wp_redirect(home_url() . '/contact?formSubmitted');
 		}
 
-        	die();   
+    	die();   
 	}
 
 	public function register_menu() {
@@ -124,6 +124,7 @@ class Sahlstroms_Miscellaneous {
 
 	public function modify_contact_methods($profile_fields) {
 		$profile_fields['phone_number'] = 'Phone Number';
+		$profile_fields['email_gateway'] = 'Email Gateway<br> <em>Used to send texts to email addresses. <br>Example: @vtext.com</em>';
 		return $profile_fields;
 	}
 
